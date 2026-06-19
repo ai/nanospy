b2369bff40b9589b250361dd480aeb8a35d50437
Andrey Sitnik <andrey@sitnik.es>
2026-06-19 11:15:21 +0000

    Remove duplicate

--- test/index.test.ts
+++ test/index.test.ts
@@ -83,29 +83,6 @@ test('resets all spies', () => {
   equal(spy2.callCount, 0)
 })
 
-test('restores spy via using keyword', { skip: !Symbol.dispose }, () => {
-  let calls: string[] = []
-  let obj = {
-    method(arg: string) {
-      calls.push(arg)
-      return arg + '!'
-    }
-  }
-  let original = obj.method
-
-  {
-    using method = spyOn(obj, 'method', arg => arg.toUpperCase() + '!')
-    equal(obj.method('a'), 'A!')
-    deepEqual(calls, [])
-    equal(method.callCount, 1)
-    equal(obj.method === original, false)
-  }
-
-  equal(obj.method === original, true)
-  equal(obj.method('b'), 'b!')
-  deepEqual(calls, ['b'])
-})
-
 test('mocks method', () => {
   let calls: string[] = []
   let obj = {
